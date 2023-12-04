007e373de3784f073259c96529519ba98eeb5614
prof-moevus <106986122+prof-moevus@users.noreply.github.com>
2023-12-04 18:46:38 -0500

    Add Counter.vue

--- venteVoitureOKLM/src/App.vue
+++ venteVoitureOKLM/src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './views/Counter.vue'
 </script>
 
 <template>
   <header>
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
--- venteVoitureOKLM/src/router/index.ts
+++ venteVoitureOKLM/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Counter from '../views/Counter.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Counter
     },
     {
       path: '/about',
